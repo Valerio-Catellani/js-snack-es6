226185b1b80fe141208979e217da3b2e2926dd17
Valerio-Catellani <valeriocatellani@gmail.com>
2024-03-22 14:21:27 +0100

    added 2/3 method for students

--- resources/js/script.js
+++ resources/js/script.js
@@ -11,32 +11,14 @@ TODO repo: js-snack-es6
  TODO nome dell'ospite (guestName),
  TODO posto occupato (place),
  TODO Generiamo e stampiamo in console la lista per i segnaposto.
-SNACK 2
-Abbiamo un elenco degli studenti di una facoltà, identificati da id, Nome e somma totale dei loro voti di esame...
-1. Per preparare l'aula di un nuovo corso, dobbiamo stampare le targhe col nome degli studenti: creare una lista contenente il loro nome tutto in maiuscolo
-ES (Marco della Rovere => MARCO DELLA ROVERE);
-2. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70
-3. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
-Buon pomeriggio e buon lavoro !!! :faccia_nerd:
-Questo è l'elenco degli studenti:
-Id  Name                Grades
-213 Marco della Rovere      78
-110 Paola Cortellessa       96
-250 Andrea Mantegna         48
-145 Gaia Borromini          74
-196 Luigi Grimaldello       68
-102 Piero della Francesca   50
-120 Francesca da Polenta    84
-Buon lavoro e Buon weekeend ! :muscolo:
+
 */
 
 //input
 //buttons
 let generateGuestInfo = document.getElementById("generate-guest-info");
 let cancelButton = document.getElementById("cancel-button");
-let addElementsButton = document.getElementById("add-elements-button")
 //output
-let response = document.getElementById("response");
 let arrayInvitati = document.querySelector(".first-array h4")
 let arraySpecifFormat = document.querySelector(".second-array h4")
 //variables
@@ -67,3 +49,93 @@ cancelButton.addEventListener('click', () => {
     arraySpecifFormat.innerHTML = "";
 })
 
+
+
+/*
+TODO SNACK 2
+TODO Abbiamo un elenco degli studenti di una facoltà, identificati da id, Nome e somma totale dei loro voti di esame...
+TODO 1. Per preparare l'aula di un nuovo corso, dobbiamo stampare le targhe col nome degli studenti: creare una lista contenente il loro nome tutto in maiuscolo
+TODO ES (Marco della Rovere => MARCO DELLA ROVERE);
+TODO 2. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70
+TODO 3. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
+TODO Buon pomeriggio e buon lavoro !!! :faccia_nerd:
+TODO Questo è l'elenco degli studenti:
+TODO Id  Name                Grades
+TODO 213 Marco della Rovere      78
+TODO 110 Paola Cortellessa       96
+TODO 250 Andrea Mantegna         48
+TODO 145 Gaia Borromini          74
+TODO 196 Luigi Grimaldello       68
+TODO 102 Piero della Francesca   50
+TODO 120 Francesca da Polenta    84
+*/
+
+
+//input
+//buttons
+let generateList = document.getElementById("generate-sudent-list");
+let cancelButton2 = document.getElementById("cancel-button-2");
+//output
+let arrayStudent = document.getElementById("array-student")
+let arrayStudentName = document.getElementById("array-student-name")
+let arrayStudentGrades = document.getElementById("array-student-grades")
+//variables
+
+let students = [
+    {
+        Id: 213,
+        Name: "Marco della Rovere",
+        Grades: 78
+    },
+    {
+        Id: 110,
+        Name: "Paola Cortellessa",
+        Grades: 96
+    },
+    {
+        Id: 250,
+        Name: "Andrea Mantegna",
+        Grades: 48
+    },
+    {
+        Id: 145,
+        Name: "Gaia Borromini",
+        Grades: 74
+    },
+    {
+        Id: 196,
+        Name: "Luigi Grimaldello",
+        Grades: 68
+    },
+    {
+        Id: 102,
+        Name: "Piero della Francesca",
+        Grades: 50
+    },
+    {
+        Id: 120,
+        Name: "Francesca da Polenta",
+        Grades: 84
+    }
+]
+
+let formattedStudent = '<pre>' + JSON.stringify(students, null, 2) + '</pre>';
+arrayStudent.innerHTML = formattedStudent;
+
+let studentName = students.map(element => {
+    return element = `<li>${element.Name.toUpperCase()}</li>`
+})
+arrayStudentName.innerHTML = `<ul>
+${studentName.join('')}
+</ul>`
+
+
+let studentWith70 = students.filter(element => {
+    return element.Grades > 70;
+}).map(element => element = `<li>${element.Name.toUpperCase()}</li>`)
+
+arrayStudentGrades.innerHTML = `<ul>
+${studentWith70.join('')}
+</ul>`
+
+let studentWith70andId120 = students.map()
